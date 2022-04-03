fdd937ca510d177ce0150e7717d15fd2ab98788f
michalkoziarski <michal.koziarski@gmail.com>
2022-04-03 17:15:37 +0200

    added support for treating variables as a real-valued numbers

--- jfots.py
+++ jfots.py
@@ -8,7 +8,6 @@ from imblearn.over_sampling.base import BaseOverSampler
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.core.algorithm import Algorithm
 from pymoo.core.problem import ElementwiseProblem
-from pymoo.factory import get_crossover, get_mutation, get_sampling
 from pymoo.indicators.hv import Hypervolume
 from pymoo.optimize import minimize
 from sklearn.base import BaseEstimator, clone
@@ -55,6 +54,17 @@ def _get_observation_types(
     return result
 
 
+def _get_feature_and_type_masks(
+    individual: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
+    individual = np.round(individual).astype(bool)
+
+    feature_mask = individual[:-4]
+    type_mask = individual[-4:]
+
+    return feature_mask, type_mask
+
+
 def _use_individual_to_resample_dataset(
     individual: np.ndarray,
     X: np.ndarray,
@@ -66,8 +76,7 @@ def _use_individual_to_resample_dataset(
     minority_class: int,
     majority_class: int,
 ) -> Optional[tuple[np.ndarray, np.ndarray]]:
-    feature_mask = individual[:-4]
-    type_mask = individual[-4:]
+    feature_mask, type_mask = _get_feature_and_type_masks(individual)
 
     # fail to resample if no features or no types were selected
     if sum(feature_mask) == 0 or sum(type_mask) == 0:
@@ -139,6 +148,7 @@ class _JFOTSProblem(ElementwiseProblem):
         oversampler_class: Type[BaseOverSampler],
         oversampler_kwargs: Optional[dict] = None,
         n_splits: int,
+        type_var: object,
     ):
         self.X = X
         self.y = y
@@ -161,13 +171,13 @@ class _JFOTSProblem(ElementwiseProblem):
         self.n_variables = X.shape[1] + 4
         self.minority_class, self.majority_class = _get_minority_and_majority_class(y)
 
-        super().__init__(n_var=self.n_variables, n_obj=2, xl=0, xu=1, type_var=bool)
+        super().__init__(n_var=self.n_variables, n_obj=2, xl=0, xu=1, type_var=type_var)
 
     def _evaluate(self, x, out, *args, **kwargs) -> None:
         precisions = []
         recalls = []
 
-        feature_mask = x[:-4]
+        feature_mask, _ = _get_feature_and_type_masks(x)
 
         for (X_train, y_train, observation_types), (X_test, y_test) in self.folds:
             resampled_dataset = _use_individual_to_resample_dataset(
@@ -207,12 +217,9 @@ class JFOTSSolution:
         objectives: np.ndarray,
     ):
         self.data = data
-        self.individual = individual
         self.objectives = objectives
 
-        self.feature_mask = individual[:-4]
-        self.type_mask = individual[-4:]
-
+        self.feature_mask, self.type_mask = _get_feature_and_type_masks(individual)
         self.used_types = {ObservationType(i): self.type_mask[i] == 1 for i in range(4)}
 
 
@@ -230,6 +237,7 @@ class JFOTS:
         optimizer_kwargs: Optional[dict] = None,
         termination: Optional[tuple] = None,
         n_splits: int = 2,
+        type_var: object = float,
         random_state: Optional[Union[int, np.random.RandomState]] = None,
         verbose: bool = False,
     ):
@@ -237,7 +245,11 @@ class JFOTS:
 
         if oversampler_class is None:
             self.oversampler_class = SMOTE
-            self.oversampler_kwargs = {"k_neighbors": 1}
+
+            if oversampler_kwargs is None:
+                self.oversampler_kwargs = {"k_neighbors": 1}
+            else:
+                self.oversampler_kwargs = oversampler_kwargs
         else:
             self.oversampler_class = oversampler_class
 
@@ -248,23 +260,17 @@ class JFOTS:
 
         if optimizer_class is None:
             self.optimizer_class = NSGA2
-            self.optimizer_kwargs = {
-                "pop_size": 100,
-                "sampling": get_sampling("bin_random"),
-                "crossover": get_crossover("bin_two_point"),
-                "mutation": get_mutation("bin_bitflip"),
-                "eliminate_duplicates": True,
-            }
         else:
             self.optimizer_class = optimizer_class
 
-            if optimizer_kwargs is None:
-                self.optimizer_kwargs = {}
-            else:
-                self.optimizer_kwargs = optimizer_kwargs
+        if optimizer_kwargs is None:
+            self.optimizer_kwargs = {}
+        else:
+            self.optimizer_kwargs = optimizer_kwargs
 
         self.termination = termination
         self.n_splits = n_splits
+        self.type_var = type_var
         self.random_state = random_state
         self.verbose = verbose
 
@@ -299,6 +305,7 @@ class JFOTS:
             oversampler_class=self.oversampler_class,
             oversampler_kwargs=self.oversampler_kwargs,
             n_splits=self.n_splits,
+            type_var=self.type_var,
         )
         optimizer = self.optimizer_class(**self.optimizer_kwargs)
         result = minimize(
@@ -312,11 +319,20 @@ class JFOTS:
 
         order = np.argsort(result.F[:, 0])
 
-        self.objectives = -result.F[order]
         self.history = [-r.opt.get("F") for r in result.history]
+        self.objectives = []
         self.solutions = []
 
-        for individual, objectives in zip(result.X[order], self.objectives):
+        processed_binarized_individuals = {}
+
+        for individual, objectives in zip(result.X[order], -result.F[order]):
+            binarized_individual = tuple(np.round(individual).astype(bool))
+
+            if processed_binarized_individuals.get(binarized_individual) is not None:
+                continue
+
+            processed_binarized_individuals[binarized_individual] = True
+
             data = _use_individual_to_resample_dataset(
                 individual,
                 X,
@@ -328,6 +344,7 @@ class JFOTS:
                 majority_class=majority_class,
             )
 
+            self.objectives.append(objectives)
             self.solutions.append(JFOTSSolution(data, individual, objectives))
 
     def hv(self, ref_point=(0.0, 0.0), normalize=False):
