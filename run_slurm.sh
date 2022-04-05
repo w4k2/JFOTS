b45ca6e96dc94105f78a0f50eb2f3a82e763c3cd
jgsirio <joanna.grzyb@sirio.app>
2022-04-05 19:55:26 +0200

    sbatch information added

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#SBATCH -P plgrid
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=72:00:00
+#SBATCH -A plgjoannagrzyb2021a
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joanna.grzyb@pwr.edu.pl
+
 
 module add plgrid/tools/python/3.8.5
 
