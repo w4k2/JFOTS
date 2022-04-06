4295986f83f3c243c4e305b8e4445d8927acdd62
joannagrzyb <joanna.grzyb@pwr.edu.pl>
2022-04-06 12:09:27 +0200

    ready to plgrid

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH -P plgrid
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=24
 #SBATCH --time=72:00:00
 #SBATCH -A plgjoannagrzyb2021a
 #SBATCH --mail-type=END
