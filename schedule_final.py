b13b4202deeae85feb8a681b4f1e56d52c4a248c
joannagrzyb <joanna.grzyb@pwr.edu.pl>
2022-04-06 12:42:29 +0200

    ready to plgrid

--- schedule_final.py
+++ schedule_final.py
@@ -4,5 +4,5 @@ import datasets
 if __name__ == '__main__':
     for fold in range(10):
         for dataset_name in datasets.names():
-            command = f'sh ./run_slurm.sh -fold {fold} -dataset_name {dataset_name}'
+            command = f'sbatch ./run_slurm.sh -fold {fold} -dataset_name {dataset_name}'
             os.system(command)
